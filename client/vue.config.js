b114154bb563e1f68741b93c9c790b05afdf2fcb
Jori <jori.kosonen@gmail.com>
2020-05-25 13:27:13 +0300

    Added logging and CORS

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
     outputDir: path.resolve(__dirname, '../server/public'),
     devServer: {
         proxy: {
+            headers: { "Access-Control-Allow-Origin": "*" },
             '^/api': {
                 target: "http://localhost:5000",
                 ws: true,
